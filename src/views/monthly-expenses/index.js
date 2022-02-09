8f2583386279e4cd4da3cbc550f11e5937a2fd79
alberto-m-santos <aslms.tic@gmail.com>
2022-02-09 23:59:26 +0000

    add expenses graphic

--- src/views/monthly-expenses/index.js
+++ src/views/monthly-expenses/index.js
@@ -1,16 +1,39 @@
+import { useEffect, useState } from 'react';
+
 // material-ui
+import { Grid } from '@mui/material';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 import EarningCard from './EarningCard';
 
+// project imports
+
+import PopularCard from './PopularCard';
+import TotalOrderLineChartCard from './TotalOrderLineChartCard';
+import TotalIncomeDarkCard from './TotalIncomeDarkCard';
+import TotalIncomeLightCard from './TotalIncomeLightCard';
+import TotalGrowthBarChart from './TotalGrowthBarChart';
+import { gridSpacing } from 'store/constant';
+
 // ==============================|| SAMPLE PAGE ||============================== //
 
 function MonthlyExpenses() {
+    const [isLoading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
     return (
-        <MainCard title="Monthly Expenses">
-            <EarningCard />
-        </MainCard>
+        <Grid container spacing={gridSpacing}>
+            <Grid item xs={12}>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item xs={12} md={8}>
+                        <TotalGrowthBarChart isLoading={isLoading} />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
