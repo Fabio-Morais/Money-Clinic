3912b5a553850aad789335a149f877f451c61c82
alberto-m-santos <aslms.tic@gmail.com>
2022-02-21 21:03:13 +0000

    update sample-page

--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -2,7 +2,7 @@
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
-import EarningCard from 'ui-component/cards/Skeleton/EarningCard';
+import EarningCard from './EarningCard';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
