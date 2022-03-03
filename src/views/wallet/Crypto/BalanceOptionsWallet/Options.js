adc4b1d15cc8172b7027f26885236ac39dc7015b
Fábio Morais <35969631+Fabio-Morais@users.noreply.github.com>
2022-03-03 23:22:47 +0000

    Add the dialog for add coin (#55)

--- src/views/wallet/Crypto/BalanceOptionsWallet/Options.js
+++ src/views/wallet/Crypto/BalanceOptionsWallet/Options.js
@@ -1,14 +1,27 @@
-import { Button, Menu, MenuItem, Stack, Typography } from '@mui/material';
+import { Button, Menu, MenuItem, Stack, Typography, useMediaQuery } from '@mui/material';
 import { MdAddCircleOutline } from 'react-icons/md';
 import { BiDotsHorizontalRounded } from 'react-icons/bi';
-import { styled } from '@mui/material/styles';
+import { styled, useTheme } from '@mui/material/styles';
 import PopupState, { bindMenu, bindTrigger } from 'material-ui-popup-state';
 import { GrEdit } from 'react-icons/gr';
+import AddCoinDialog from './Dialogs/AddCoinDialog';
+import { useState } from 'react';
 
 const GreyButton = styled(Button)(({ theme }) => ({
     '&:hover': { backgroundColor: theme.palette.grey['300'] }
 }));
 function Options() {
+    const [open, setOpen] = useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <Stack direction="row" spacing={1}>
             <PopupState variant="popover" popupId="demo-popup-menu">
@@ -31,9 +44,15 @@ function Options() {
                     </>
                 )}
             </PopupState>
-            <Button variant="contained" startIcon={<MdAddCircleOutline />} sx={{ backgroundColor: 'primary.800' }}>
+            <Button
+                variant="contained"
+                startIcon={<MdAddCircleOutline />}
+                sx={{ backgroundColor: 'primary.800' }}
+                onClick={handleClickOpen}
+            >
                 Add new
             </Button>
+            <AddCoinDialog fullScreen={fullScreen} handleClose={handleClose} open={open} />
         </Stack>
     );
 }
